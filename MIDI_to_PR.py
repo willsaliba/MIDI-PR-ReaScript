e6fa1a521c5a460e0f6c0435dd9f43d52c19f0e7
Will Saliba <salibawill@gmail.com>
2023-10-05 13:54:52 +1000

    ability to save MIDI segments

--- MIDI_to_PR.py
+++ MIDI_to_PR.py
@@ -3,6 +3,7 @@ RPR_ClearConsole()
 #checking if file selected
 numSelected = RPR_CountSelectedMediaItems(0)
 isMIDI = False
+
 if numSelected == 1:
   
   #if selected extract info from segment
@@ -10,13 +11,20 @@ if numSelected == 1:
   take = RPR_GetTake(selectedItem, 0)
   isMIDI = RPR_TakeIsMIDI(take)
   
-  
 #output if MIDI
 RPR_ShowConsoleMsg("MIDI FILE SELECTED: ")
 if isMIDI:
-  RPR_ShowConsoleMsg("TRUE")
+  RPR_ShowConsoleMsg("TRUE\n")
+  
+  #set location to save MIDI
+  save_path = "/Users/willsaliba/Downloads/hooray.midi"
+  
+  #save MIDI file to selected location ??
+  PCM_src = RPR_GetMediaItemTake_Source( take )
+  #res = RPR_CF_ExportMediaSource( PCM, save_path )
+  
+  
 else: 
-  RPR_ShowConsoleMsg("FALSE")
+  RPR_ShowConsoleMsg("FALSE\n")
+ 
 
-
-#save MIDI file to "/Users/willsaliba/Downloads"
