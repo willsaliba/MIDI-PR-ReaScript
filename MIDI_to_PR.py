754e16a3225e7794ac42e511590589adfe9736e5
Will Saliba <salibawill@gmail.com>
2023-10-10 23:57:40 +1030

    adding script which converts midi file to instrument rolls

--- MIDI_to_PR.py
+++ MIDI_to_PR.py
@@ -1,9 +1,11 @@
 from sws_python import CF_ExportMediaSource
 RPR_ClearConsole()
 
+isMIDI = False
+save_succeded = False
+
 #checking if file selected
 numSelected = RPR_CountSelectedMediaItems(0)
-isMIDI = False
 
 #if selected extract info from segment
 if numSelected == 1:
@@ -12,31 +14,35 @@ if numSelected == 1:
   isMIDI = RPR_TakeIsMIDI(take)
   
 #If MIDI try to save segment
-RPR_ShowConsoleMsg("MIDI FILE SELECTED: ")
 if isMIDI:
-  RPR_ShowConsoleMsg("TRUE\n")
+  RPR_ShowConsoleMsg("MIDI FILE SELECTED\n")
   
   #set location to save MIDI
-  save_path = "/Users/willsaliba/Downloads/topics/reaper/M_to_PR/hooray.midi"
+  save_dir = "/Users/willsaliba/Documents/Topics/ReaScripts/outputs"
+  midi_path = save_dir + "/original.mid"
+  
 
   #saving MIDI segment using SWS function (with take & pcm_src)
   PCM_src = RPR_GetMediaItemTake_Source(take)
-  save_succeded = CF_ExportMediaSource(PCM_src, save_path) 
+  save_succeded = CF_ExportMediaSource(PCM_src, midi_path)
   
 else: 
-  RPR_ShowConsoleMsg("FALSE\n")
+  RPR_ShowConsoleMsg("MIDI FILE NOT SELECTED\n")
   
 #if save succeeded convert to piano roll
 if save_succeded:
 
-  RPR_ShowConsoleMsg("\n\nFILE SAVED TO: " + save_path)
-  
+  RPR_ShowConsoleMsg("\n\nFILE SAVED TO: " + midi_path)
   RPR_ShowConsoleMsg("\n\nConverting to Piano Roll...")
-  #!!! insert jacks code here
   
+  #INSERT SCRIPT.PY HERE
+  
+  
+  
+  #RPR_ShowConsoleMsg("\n\nINSTRUMENT ROLLS SAVED")
   
 else:
-  RPR_ShowConsoleMsg("\n\nFILE FAILED TO SAVE")
+  RPR_ShowConsoleMsg("\n\nNO ACTION TAKEN")
  
 
 
